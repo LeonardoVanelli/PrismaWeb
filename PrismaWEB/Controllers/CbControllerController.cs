58f8b809c30438f13ae86cae5bff6400aa0bdce2
leonardo vanelli <Leonardo.Vanelli123@gmail.com>
2018-03-05 21:37:43 -0300

    Arquivos organizados

--- PrismaWEB/Controllers/CbControllerController.cs
+++ PrismaWEB/Controllers/CbControllerController.cs
@@ -9,16 +9,9 @@ namespace PrismaWEB.Controllers
 {
     public class CbControllerController : Controller
     {
-        private PrismaEntities db = new PrismaEntities();
         // GET: CbController
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult Localizacao()
-        {
-            ViewBag.Pais_Id = new SelectList(db.PAISES, "Id", "Nome");
             return View();
         }
     }

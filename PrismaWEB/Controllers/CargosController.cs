38c818b5c7f02dcd4c6b11f6f2eb1ca368d14835
leonardo vanelli <Leonardo.Vanelli123@gmail.com>
2018-03-06 18:47:16 -0300

    Criado CRUD Cargos

---
+++ PrismaWEB/Controllers/CargosController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PrismaWEB.Models;
+
+namespace PrismaWEB.Controllers
+{
+    public class CargosController : Controller
+    {
+        private PrismaEntities db = new PrismaEntities();
+
+        // GET: Cargos
+        public ActionResult Index()
+        {
+            return View(db.CARGOS.ToList());
+        }
+
+        // GET: Cargos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CARGOS cARGOS = db.CARGOS.Find(id);
+            if (cARGOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cARGOS);
+        }
+
+        // GET: Cargos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cargos/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Descricao,DataCraicao,DataAlteracao")] CARGOS cARGOS)
+        {
+            cARGOS.Id = cARGOS.GetHashCode();
+            cARGOS.DataCraicao = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                db.CARGOS.Add(cARGOS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cARGOS);
+        }
+
+        // GET: Cargos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CARGOS cARGOS = db.CARGOS.Find(id);
+            if (cARGOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cARGOS);
+        }
+
+        // POST: Cargos/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CARGOS Cargo)
+        {
+            if (ModelState.IsValid)
+            {            
+                Cargo.DataAlteracao = DateTime.Now;
+                db.Entry(Cargo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Cargo);
+        }
+
+        // GET: Cargos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CARGOS cARGOS = db.CARGOS.Find(id);
+            if (cARGOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cARGOS);
+        }
+
+        // POST: Cargos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CARGOS cARGOS = db.CARGOS.Find(id);
+            db.CARGOS.Remove(cARGOS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
